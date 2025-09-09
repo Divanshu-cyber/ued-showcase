9b6f5d3687ba7e5ddf1515e22abf870ab81369cc
Divanshu-cyber <divanshu.sharma@gspann.com>
2025-09-09 16:28:43 +0530

    added quote in defination, model and filter

---
+++ blocks/hero/hero.js
@@ -0,0 +1,6 @@
+ const val = document.querySelector('.hero.block > div > div > picture')
+    console.log('hero block : ',val);
+
+async function decorate(block){ 
+    console.log('decorate hero block : ',block);
+}
